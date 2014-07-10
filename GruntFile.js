d0ceea620678491972d235a57218d3162ff43823
Alex Banks <alex@moonhighway.com>
2014-07-10 12:26:44 -0700

    Adds grunt minified Game File

---
+++ GruntFile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            dist: {
+                files: {
+                    'public/js/dist/<%= pkg.name %>.min.js': ['public/js/*.js'],
+                    'public/test/dist/<%= pkg.name %>.tests.min.js': ['public/test/*.js']
+                }
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
+
+};
