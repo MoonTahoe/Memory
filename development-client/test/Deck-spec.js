5d80c25f86bbf431ec8b84c153f1b1baa91772f5
Alex Banks <alex@moonhighway.com>
2014-07-11 17:35:33 -0700

    Adds a better shuffle test, by adding 100 cards instead of 4

--- development-client/test/Deck-spec.js
+++ development-client/test/Deck-spec.js
@@ -1,34 +1,36 @@
 /* global Deck, Card */
 
-describe("Game Deck", function() {
+describe("Game Deck", function () {
 
-    beforeEach(function() {
+    beforeEach(function () {
         this.deck = new Deck();
-        var deck = this.deck;
-        this.sampleCards = ["Ace", "King", "Queen", "Jack"];
-        this.sampleCards.forEach(function(sampleCard) {
+        var i, deck = this.deck;
+        this.sampleCards = [];
+        for (i = 0; i < 100; i++) {
+            this.sampleCards.push(i.toString());
+        }
+        this.sampleCards.forEach(function (sampleCard) {
             deck.GameCards.push(new Card(sampleCard));
         });
-
     });
 
-    it("can be created", function() {
+    it("can be created", function () {
         expect(this.deck).toBeDefined();
     });
 
-    it("can hold cards", function() {
+    it("can hold cards", function () {
         var deck = this.deck;
         expect(deck.GameCards.length).toEqual(this.sampleCards.length);
-        this.sampleCards.forEach(function(sampleCard, i) {
+        this.sampleCards.forEach(function (sampleCard, i) {
             expect(deck.GameCards[i].FaceValue.toString()).toEqual(sampleCard);
         });
     });
 
-    it("can shuffle", function() {
+    it("can shuffle", function () {
         this.deck.shuffle();
         var shuffled = [];
 
-        this.deck.GameCards.forEach(function(card) {
+        this.deck.GameCards.forEach(function (card) {
             shuffled.push(card.FaceValue);
         });
 
