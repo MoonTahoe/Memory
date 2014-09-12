6040bc30ca717b4fa7951a30de46882f5963a364
Alex Banks <alex@moonhighway.com>
2014-09-12 15:22:08 -0700

    Incorporates Grunt Bump

--- GruntFile.js
+++ GruntFile.js
@@ -43,9 +43,25 @@ module.exports = function (grunt) {
                     "production-client/css/style.css": "development-client/css/style.less"
                 }
             }
+        },
+        bump: {
+            options: {
+                files: ['package.json'],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['package.json'],
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'https://github.com/MoonTahoe/craps-cli.git',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
+            }
         }
     });
 
+    grunt.loadNpmTasks('grunt-bump');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-jasmine');
